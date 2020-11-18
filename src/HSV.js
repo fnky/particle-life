1d17538ef2d23dd544a441ca16935f7c0bfd3129
Christian Petersen <christianpetersen@journiapp.com>
2020-11-18 20:43:28 +0100

    chore: reformat files

--- src/HSV.js
+++ src/HSV.js
@@ -1,51 +1,51 @@
 function fromHSV(h, s, v) {
-	const i = Math.round(h * 6);
-	const f = h * 6 - i;
-	const p = v * (1 - s);
-	const q = v * (1 - f * s);
-	const t = v * (1 - (1 - f) * s);
+  const i = Math.round(h * 6);
+  const f = h * 6 - i;
+  const p = v * (1 - s);
+  const q = v * (1 - f * s);
+  const t = v * (1 - (1 - f) * s);
 
-	let r;
-	let g;
-	let b;
-	switch (i % 6) {
-		case 0:
-			r = v;
-			g = t;
-			b = p;
-			break;
-		case 1:
-			r = q;
-			g = v;
-			b = p;
-			break;
-		case 2:
-			r = p;
-			g = v;
-			b = t;
-			break;
-		case 3:
-			r = p;
-			g = q;
-			b = v;
-			break;
-		case 4:
-			r = t;
-			g = p;
-			b = v;
-			break;
-		case 5:
-			r = v;
-			g = p;
-			b = q;
-			break;
-	}
+  let r;
+  let g;
+  let b;
+  switch (i % 6) {
+    case 0:
+      r = v;
+      g = t;
+      b = p;
+      break;
+    case 1:
+      r = q;
+      g = v;
+      b = p;
+      break;
+    case 2:
+      r = p;
+      g = v;
+      b = t;
+      break;
+    case 3:
+      r = p;
+      g = q;
+      b = v;
+      break;
+    case 4:
+      r = t;
+      g = p;
+      b = v;
+      break;
+    case 5:
+      r = v;
+      g = p;
+      b = q;
+      break;
+  }
 
-	return {
-		r: Math.round(r * 255),
-		g: Math.round(g * 255),
-		b: Math.round(b * 255),
-	};
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255),
+  };
 }
 
 module.exports.fromHSV = fromHSV;
--- src/index.js
+++ src/index.js
@@ -13,214 +13,214 @@ const entropyFolder = gui.addFolder('Entropy');
 const stepsPerFrameNormal = 1;
 
 const settings = {
-	animate: true,
-	scaleToFit: true,
-	scaleToView: true,
-	loop: false,
+  animate: true,
+  scaleToFit: true,
+  scaleToView: true,
+  loop: false,
 };
 
 const engines = {
-	MersenneTwister19937: (() => {
-		const generator = MersenneTwister19937.autoSeed();
-		return generator.next.bind(generator);
-	})(),
-	WebCrypto: browserCrypto.next,
+  MersenneTwister19937: (() => {
+    const generator = MersenneTwister19937.autoSeed();
+    return generator.next.bind(generator);
+  })(),
+  WebCrypto: browserCrypto.next,
 };
 
 const presets = {
-	Balanced: {
-		population: [9, 400],
-		seed: [-0.02, 0.06, 0.0, 20.0, 20.0, 70.0, 0.05, false],
-	},
-	Chaos: {
-		population: [6, 400],
-		seed: [0.02, 0.04, 0.0, 30.0, 30.0, 100.0, 0.01, false],
-	},
-	Diversity: {
-		population: [12, 400],
-		seed: [-0.01, 0.04, 0.0, 20.0, 10.0, 60.0, 0.05, true],
-	},
-	Frictionless: {
-		population: [6, 300],
-		seed: [0.01, 0.005, 10.0, 10.0, 10.0, 60.0, 0.0, true],
-	},
-	Gliders: {
-		population: [6, 400],
-		seed: [0.0, 0.06, 0.0, 20.0, 10.0, 50.0, 0.1, true],
-	},
-	Homogeneity: {
-		population: [4, 400],
-		seed: [0.0, 0.04, 10.0, 10.0, 10.0, 80.0, 0.05, true],
-	},
-	'Large Clusters': {
-		population: [6, 400],
-		seed: [0.025, 0.02, 0.0, 30.0, 30.0, 100.0, 0.2, false],
-	},
-	'Medium Clusters': {
-		population: [6, 400],
-		seed: [0.02, 0.05, 0.0, 20.0, 20.0, 50.0, 0.05, false],
-	},
-	Quiescence: {
-		population: [6, 300],
-		seed: [-0.02, 0.1, 10.0, 20.0, 20.0, 60.0, 0.2, false],
-	},
-	'Small Clusters': {
-		population: [6, 600],
-		seed: [-0.005, 0.01, 10.0, 10.0, 20.0, 50.0, 0.01, false],
-	},
+  Balanced: {
+    population: [9, 400],
+    seed: [-0.02, 0.06, 0.0, 20.0, 20.0, 70.0, 0.05, false],
+  },
+  Chaos: {
+    population: [6, 400],
+    seed: [0.02, 0.04, 0.0, 30.0, 30.0, 100.0, 0.01, false],
+  },
+  Diversity: {
+    population: [12, 400],
+    seed: [-0.01, 0.04, 0.0, 20.0, 10.0, 60.0, 0.05, true],
+  },
+  Frictionless: {
+    population: [6, 300],
+    seed: [0.01, 0.005, 10.0, 10.0, 10.0, 60.0, 0.0, true],
+  },
+  Gliders: {
+    population: [6, 400],
+    seed: [0.0, 0.06, 0.0, 20.0, 10.0, 50.0, 0.1, true],
+  },
+  Homogeneity: {
+    population: [4, 400],
+    seed: [0.0, 0.04, 10.0, 10.0, 10.0, 80.0, 0.05, true],
+  },
+  'Large Clusters': {
+    population: [6, 400],
+    seed: [0.025, 0.02, 0.0, 30.0, 30.0, 100.0, 0.2, false],
+  },
+  'Medium Clusters': {
+    population: [6, 400],
+    seed: [0.02, 0.05, 0.0, 20.0, 20.0, 50.0, 0.05, false],
+  },
+  Quiescence: {
+    population: [6, 300],
+    seed: [-0.02, 0.1, 10.0, 20.0, 20.0, 60.0, 0.2, false],
+  },
+  'Small Clusters': {
+    population: [6, 600],
+    seed: [-0.005, 0.01, 10.0, 10.0, 20.0, 50.0, 0.01, false],
+  },
 };
 
 function getSettingsForPreset(preset) {
-	const { population, seed } = presets[preset];
-	const [numTypes, numParticles] = population;
-	const [
-		attractMean,
-		attractStd,
-		minRLower,
-		minRUpper,
-		maxRLower,
-		maxRUpper,
-		friction,
-		flatForce,
-	] = seed;
+  const { population, seed } = presets[preset];
+  const [numTypes, numParticles] = population;
+  const [
+    attractMean,
+    attractStd,
+    minRLower,
+    minRUpper,
+    maxRLower,
+    maxRUpper,
+    friction,
+    flatForce,
+  ] = seed;
 
-	return {
-		numTypes,
-		numParticles,
-		attractMean,
-		attractStd,
-		minRLower,
-		minRUpper,
-		maxRLower,
-		maxRUpper,
-		friction,
-		flatForce,
-	};
+  return {
+    numTypes,
+    numParticles,
+    attractMean,
+    attractStd,
+    minRLower,
+    minRUpper,
+    maxRLower,
+    maxRUpper,
+    friction,
+    flatForce,
+  };
 }
 
 const sketch = ({ width, height }) => {
-	const defaultPreset = 'Chaos';
+  const defaultPreset = 'Chaos';
 
-	const universeSettings = {
-		...getSettingsForPreset(defaultPreset),
-		preset: defaultPreset,
-		wrap: false,
-	};
+  const universeSettings = {
+    ...getSettingsForPreset(defaultPreset),
+    preset: defaultPreset,
+    wrap: false,
+  };
 
-	const renderSettings = {
-		stepsPerFrame: stepsPerFrameNormal,
-	};
+  const renderSettings = {
+    stepsPerFrame: stepsPerFrameNormal,
+  };
 
-	const entropySettings = {
-		engine: 'MersenneTwister19937',
-	};
+  const entropySettings = {
+    engine: 'MersenneTwister19937',
+  };
 
-	const universe = new Universe(
-		universeSettings.numTypes,
-		universeSettings.numParticles,
-		width,
-		height
-	);
-	universe.wrap = universeSettings.wrap;
-	universe.reSeed(
-		universeSettings.attractMean,
-		universeSettings.attractStd,
-		universeSettings.minRLower,
-		universeSettings.minRUpper,
-		universeSettings.maxRLower,
-		universeSettings.maxRUpper,
-		universeSettings.friction,
-		universeSettings.flatForce
-	);
+  const universe = new Universe(
+    universeSettings.numTypes,
+    universeSettings.numParticles,
+    width,
+    height
+  );
+  universe.wrap = universeSettings.wrap;
+  universe.reSeed(
+    universeSettings.attractMean,
+    universeSettings.attractStd,
+    universeSettings.minRLower,
+    universeSettings.minRUpper,
+    universeSettings.maxRLower,
+    universeSettings.maxRUpper,
+    universeSettings.friction,
+    universeSettings.flatForce
+  );
 
-	const onUniverseSettingsChange = () => {
-		universe.setPopulation(
-			universeSettings.numTypes,
-			universeSettings.numParticles
-		);
-		universe.reSeed(
-			universeSettings.attractMean,
-			universeSettings.attractStd,
-			universeSettings.minRLower,
-			universeSettings.minRUpper,
-			universeSettings.maxRLower,
-			universeSettings.maxRUpper,
-			universeSettings.friction,
-			universeSettings.flatForce
-		);
-	};
+  const onUniverseSettingsChange = () => {
+    universe.setPopulation(
+      universeSettings.numTypes,
+      universeSettings.numParticles
+    );
+    universe.reSeed(
+      universeSettings.attractMean,
+      universeSettings.attractStd,
+      universeSettings.minRLower,
+      universeSettings.minRUpper,
+      universeSettings.maxRLower,
+      universeSettings.maxRUpper,
+      universeSettings.friction,
+      universeSettings.flatForce
+    );
+  };
 
-	const onPresetChange = () => {
-		Object.assign(
-			universeSettings,
-			getSettingsForPreset(universeSettings.preset)
-		);
-		universeFolder.updateDisplay();
-		onUniverseSettingsChange();
-	};
+  const onPresetChange = () => {
+    Object.assign(
+      universeSettings,
+      getSettingsForPreset(universeSettings.preset)
+    );
+    universeFolder.updateDisplay();
+    onUniverseSettingsChange();
+  };
 
-	const onEngineChange = () => {
-		universe.setEngine(
-			engines[entropySettings.engine] || engines.MersenneTwister19937
-		);
-		onUniverseSettingsChange();
-	};
+  const onEngineChange = () => {
+    universe.setEngine(
+      engines[entropySettings.engine] || engines.MersenneTwister19937
+    );
+    onUniverseSettingsChange();
+  };
 
-	universeFolder
-		.add(universeSettings, 'attractMean', -1, 1)
-		.onFinishChange(onUniverseSettingsChange);
-	universeFolder
-		.add(universeSettings, 'attractStd', -1, 1)
-		.onFinishChange(onUniverseSettingsChange);
-	universeFolder
-		.add(universeSettings, 'minRLower', 0, 100)
-		.onFinishChange(onUniverseSettingsChange);
-	universeFolder
-		.add(universeSettings, 'minRUpper', 0, 100)
-		.onFinishChange(onUniverseSettingsChange);
-	universeFolder
-		.add(universeSettings, 'maxRLower', 0, 100)
-		.onFinishChange(onUniverseSettingsChange);
-	universeFolder
-		.add(universeSettings, 'maxRUpper', 0, 100)
-		.onFinishChange(onUniverseSettingsChange);
-	universeFolder
-		.add(universeSettings, 'friction', 0, 1)
-		.onFinishChange(onUniverseSettingsChange);
-	universeFolder
-		.add(universeSettings, 'flatForce')
-		.onFinishChange(onUniverseSettingsChange);
-	universeFolder.add(universeSettings, 'wrap').onChange(() => {
-		universe.wrap = universeSettings.wrap;
-	});
-	universeFolder
-		.add(universeSettings, 'preset', Object.keys(presets))
-		.onFinishChange(onPresetChange);
-	universeFolder.add(
-		{
-			randomParticles: () => {
-				universe.setRandomParticles();
-			},
-		},
-		'randomParticles'
-	);
+  universeFolder
+    .add(universeSettings, 'attractMean', -1, 1)
+    .onFinishChange(onUniverseSettingsChange);
+  universeFolder
+    .add(universeSettings, 'attractStd', -1, 1)
+    .onFinishChange(onUniverseSettingsChange);
+  universeFolder
+    .add(universeSettings, 'minRLower', 0, 100)
+    .onFinishChange(onUniverseSettingsChange);
+  universeFolder
+    .add(universeSettings, 'minRUpper', 0, 100)
+    .onFinishChange(onUniverseSettingsChange);
+  universeFolder
+    .add(universeSettings, 'maxRLower', 0, 100)
+    .onFinishChange(onUniverseSettingsChange);
+  universeFolder
+    .add(universeSettings, 'maxRUpper', 0, 100)
+    .onFinishChange(onUniverseSettingsChange);
+  universeFolder
+    .add(universeSettings, 'friction', 0, 1)
+    .onFinishChange(onUniverseSettingsChange);
+  universeFolder
+    .add(universeSettings, 'flatForce')
+    .onFinishChange(onUniverseSettingsChange);
+  universeFolder.add(universeSettings, 'wrap').onChange(() => {
+    universe.wrap = universeSettings.wrap;
+  });
+  universeFolder
+    .add(universeSettings, 'preset', Object.keys(presets))
+    .onFinishChange(onPresetChange);
+  universeFolder.add(
+    {
+      randomParticles: () => {
+        universe.setRandomParticles();
+      },
+    },
+    'randomParticles'
+  );
 
-	renderingFolder.add(renderSettings, 'stepsPerFrame', 1, 10);
+  renderingFolder.add(renderSettings, 'stepsPerFrame', 1, 10);
 
-	entropyFolder
-		.add(entropySettings, 'engine', Object.keys(engines))
-		.onFinishChange(onEngineChange);
+  entropyFolder
+    .add(entropySettings, 'engine', Object.keys(engines))
+    .onFinishChange(onEngineChange);
 
-	return ({ context, width, height }) => {
-		context.fillStyle = 'black';
-		context.fillRect(0, 0, width, height);
+  return ({ context, width, height }) => {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
 
-		for (let i = 0; i < renderSettings.stepsPerFrame; ++i) {
-			const opacity = (i + 1) / renderSettings.stepsPerFrame;
-			universe.step();
-			universe.draw(context, opacity);
-		}
-	};
+    for (let i = 0; i < renderSettings.stepsPerFrame; ++i) {
+      const opacity = (i + 1) / renderSettings.stepsPerFrame;
+      universe.step();
+      universe.draw(context, opacity);
+    }
+  };
 };
 
 canvasSketch(sketch, settings);
